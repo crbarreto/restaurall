7899f6774311df9043b5be56202d9de56d471328
kamiloC <kamilo.sniper@gmail.com>
2018-10-17 17:09:41 -0500

    Feat: Add google places and current location

--- 1.Presentation/userApp/src/screens/HomeScreen.js
+++ 1.Presentation/userApp/src/screens/HomeScreen.js
@@ -11,10 +11,18 @@ import {
   View,
   Dimensions,
 } from 'react-native';
+import {
+  Searchbar,
+  Dialog,
+  Portal,
+  Paragraph,
+  Button,
+  Modal,
+  Divider
+} from 'react-native-paper';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import SplashScreen from 'react-native-splash-screen';
-import MapView from 'react-native-maps';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Searchbar } from 'react-native-paper';
 import RNGooglePlaces from 'react-native-google-places';
 
 // Components
@@ -26,10 +34,24 @@ import { COLORS } from '../constants/Theme';
 const { height, width } = Dimensions.get('window');
 type Props = {};
 type State = {
-  searchAddress: String
+  searchAddress: String,
+  position: {
+    latitude: Number,
+    longitud: Number
+  },
+  isDialogVisible: boolean,
+  dialogContent: {
+    title: String,
+    content: String,
+    mainButtonTitle: String,
+    mainAction: VoidFunction,
+  },
+  searchResults: Array,
+  isSearchResultsViewVisible: boolean,
 };
 
 export default class HomeScreen extends React.Component<Props, State> {
+
   static navigationOptions = {
     title: 'Pantalla principal',
     drawerIcon: ({ tintColor }) => (
@@ -42,58 +64,186 @@ export default class HomeScreen extends React.Component<Props, State> {
 
     this.state = {
       searchAddress: '',
+      position: {
+        latitude: 4.653203,
+        longitude: -74.061226,
+      },
+      isDialogVisible: false,
+      dialogContent: {
+        title: '',
+        content: '',
+        mainButtonTitle: 'Entendido',
+        mainAction: () => false,
+      },
+      searchResults: [],
+      isSearchResultsViewVisible: false,
     };
 
     this.autocompletSearchWithPlaces = this.autocompletSearchWithPlaces.bind(this);
+    this.getCurrentPositionAndUpdateMap = this.getCurrentPositionAndUpdateMap.bind(this);
+    this.showDialog = this.showDialog.bind(this);
+    this.hideDialog = this.hideDialog.bind(this);
+  }
+
+  autocompletSearchWithPlaces(query: string) {
+    const { latitude, longitude } = this.state.position;
+    const searchOptions = {
+      type: 'establishment',
+      country: 'CO',
+      latitude,
+      longitude,
+      radius: 20
+    };
+
+    this.setState({ searchAddress: query });
+
+    RNGooglePlaces.getAutocompletePredictions(query, searchOptions)
+      .then((places) => {
+        this.setState({
+          searchResults: places.map((place) => {
+            return { name: place.fullText.split(',')[0], id: place.placeID };
+          })
+        });
+      })
+      .catch(error => console.log(error.message));
+  }
+
+  getCurrentPositionAndUpdateMap() {
+
+    this.hideDialog();
+
+    navigator.geolocation.getCurrentPosition(
+      (currentPosition) => {
+        const { coords } = currentPosition;
+        if (coords)
+          this.setState({
+            position: coords,
+          });
+      }, (error) => {
+        this.showDialog({
+          title: 'Error',
+          content: 'Tuvimos un error al obtener tu ubicación, intenta de nuevo.',
+          mainButtonTitle: 'Reintentar',
+          mainAction: this.getCurrentPositionAndUpdateMap
+        });
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+      }
+    );
+  }
+
+  showDialog({ title, content, mainButtonTitle, mainAction }) {
+    this.setState({
+      dialogContent: {
+        title,
+        content,
+        mainButtonTitle,
+        mainAction,
+      },
+      isDialogVisible: true,
+    });
+  }
+
+  hideDialog() {
+    this.setState({
+      isDialogVisible: false,
+    });
   }
 
   componentDidMount() {
     // Hide SplashScreen
     SplashScreen.hide();
+    // Get user position
+    this.getCurrentPositionAndUpdateMap();
   }
 
-  autocompletSearchWithPlaces(query: string) {
-    this.setState({ searchAddress: query });
-
-    RNGooglePlaces.getAutocompletePredictions(query)
-      .then((place) => {
-        console.log(place);
-        // place represents user's selection from the
-        // suggestions and it is a simplified Google Place object.
-      })
-      .catch(error => console.log(error.message));
+  componentDidUpdate() {
+    if (this.refs['userLocationMarker'])
+      this.refs['userLocationMarker'].showCallout();
   }
 
   render() {
 
-    const { searchAddress } = this.state;
+    const {
+      searchAddress,
+      position,
+      isDialogVisible,
+      dialogContent,
+      searchResults,
+      isSearchResultsViewVisible
+    } = this.state;
 
     return (
       <View style={styles.container}>
+
         <View style={{ zIndex: 2 }}>
           <Header navigation={this.props.navigation} />
 
           <Searchbar
-            style={{ marginHorizontal: '5%', width: '90%' }}
+            ref="searchbar"
+            style={styles.searchAreaItem}
             placeholder="Búsqueda"
             onChangeText={this.autocompletSearchWithPlaces}
             value={searchAddress}
           />
 
+          {
+            searchResults.length ?
+              <View style={[styles.searchAreaItem, { backgroundColor: '#fff', paddingHorizontal: '2%', paddingTop: '2%' }]}>
+                {
+                  searchResults.map((place, index) => {
+                    return (
+                      <View key={place.id} style={styles.searchResultItem}>
+                        <RegularText style={styles.searchResultItemText}>{place.name}</RegularText>
+                        {(index + 1) === searchResults.length ? null : <Divider />}
+                      </View>
+                    );
+                  })
+                }
+              </View>
+              : null
+          }
         </View>
 
         <MapView
           style={styles.map}
-
           region={{
-            latitude: 4.653203,
-            longitude: -74.061226,
+            latitude: position.latitude,
+            longitude: position.longitude,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
-          }}
-        />
+          }}>
 
-      </View>
+          <Marker
+            ref="userLocationMarker"
+            calloutAnchor={{ x: 0.5, y: -0.1 }}
+            coordinate={position}
+            image={require('../assets/icons/pin.png')} >
+            <Callout tooltip style={styles.calloutContainer} >
+              <BoldText style={styles.calloutText}>Ubicación actual</BoldText>
+            </Callout>
+          </Marker>
+
+        </MapView>
+
+        <Portal>
+          <Dialog
+            visible={isDialogVisible}
+            onDismiss={this.hideDialog}>
+            <Dialog.Title>{dialogContent.title}</Dialog.Title>
+            <Dialog.Content>
+              <Paragraph>{dialogContent.content}</Paragraph>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={dialogContent.mainAction}>{dialogContent.mainButtonTitle}</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+
+      </View >
     );
   }
 }
@@ -104,5 +254,27 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  searchAreaItem: {
+    marginHorizontal: '5%',
+    width: '90%',
+  },
+  calloutContainer: {
+    height: 25,
+    paddingBottom: 5,
+    width: 120,
+  },
+  calloutText: {
+    alignSelf: 'center',
+    backgroundColor: COLORS.main,
+    color: '#fff',
+    padding: 4,
+  },
+  searchResultItem: {
+    marginVertical: '2%',
+  },
+  searchResultItemText: {
+    fontSize: 16,
+    paddingVertical: '1%'
   },
 });
